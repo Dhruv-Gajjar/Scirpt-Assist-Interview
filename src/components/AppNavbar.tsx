de1cef59c8d861c24437f84ea1df6465e015f798
Dhruv Gajjar <dhruvgajjar07@gmail.com>
2025-02-20 17:23:24 +0530

    Update AppNavbar.tsx

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Flex, Paper, Text } from "@mantine/core";
 import { FC, ReactNode } from "react";
 import { MdLogout } from "react-icons/md";
+import { Link } from "react-router-dom";
 import useAuthStore from "../store/auth-store";
 
 interface AppNavbarProps {
@@ -14,9 +15,11 @@ const AppNavbar: FC = () => {
     <Box maw="100vw">
       <Paper bg="gray.3" shadow="xl" radius={0}>
         <Flex align="center" justify="space-between" p={12}>
-          <Text fw="bold" size={28}>
-            StarWars
-          </Text>
+          <Link to={"/"}>
+            <Text fw="bold" size={28}>
+              StarWars
+            </Text>
+          </Link>
           <Button onClick={logout} rightIcon={<MdLogout size={20} />}>
             Logout
           </Button>
