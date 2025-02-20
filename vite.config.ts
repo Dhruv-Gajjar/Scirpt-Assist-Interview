c944006879e2c1d6790afcb1be0690cb94c19613
Dhruv Gajjar <dhruvgajjar07@gmail.com>
2025-02-20 17:06:39 +0530

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	server: {
-		port: 5175
-	},
-	plugins: [react()],
-	build: {
-		outDir: 'build'
-	}
+  server: {
+    port: 5175,
+  },
+  plugins: [react()],
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
