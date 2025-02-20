8c3356670f608743f887aa808cb730de057f6d23
Dhruv Gajjar <dhruvgajjar07@gmail.com>
2025-02-20 16:12:26 +0530

    Update Species UI and Add filter

--- src/pages/Species/Species.tsx
+++ src/pages/Species/Species.tsx
@@ -1,6 +1,10 @@
 import {
+  Button,
   Center,
   Container,
+  Flex,
+  Input,
+  Loader,
   Pagination,
   Table,
   Text,
@@ -8,7 +12,8 @@ import {
 } from "@mantine/core";
 import { usePagination } from "@mantine/hooks";
 import { useQuery } from "@tanstack/react-query";
-import { FC, useEffect, useState } from "react";
+import { ChangeEvent, FC, useMemo, useState } from "react";
+import { MdChevronLeft, MdSearch } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import { ISpecie } from "swapi-ts";
 import { getSpeciesById, getStarWarsData } from "../../services/api";
@@ -16,13 +21,14 @@ import { useAppStore } from "../../store/app.store";
 
 const Species: FC = () => {
   const [selectedSpecie, setSelectedSpecie] = useState<string>("");
+  const [search, setSearch] = useState<string>("");
 
   const { species, setSpeciesData, setSpecieDetailData } = useAppStore();
   const navigate = useNavigate();
   const totalPages = Math.ceil((species.count || 0) / 10);
   const pagination = usePagination({ total: totalPages, initialPage: 1 });
 
-  const { data: speciesData, isLoading: sepciesLoading } = useQuery({
+  const { data: speciesData, isFetching: speciesLoading } = useQuery({
     queryKey: ["species", pagination.active],
     queryFn: async () => {
       const baseUrl = "https://swapi.dev/api/species/";
@@ -35,11 +41,13 @@ const Species: FC = () => {
       return response;
     },
     keepPreviousData: true,
+    staleTime: 0.5,
+    cacheTime: 5 * 60 * 1000,
     enabled: !!pagination.active,
   });
 
-  const { isLoading: selectedPlanetLoading } = useQuery({
-    queryKey: ["people", selectedSpecie],
+  const { isFetching: selectedSpecieLoading } = useQuery({
+    queryKey: ["specie", selectedSpecie],
     queryFn: async () => {
       const data: ISpecie = await getSpeciesById(selectedSpecie);
       if (data) {
@@ -49,49 +57,107 @@ const Species: FC = () => {
       }
       return data;
     },
+    staleTime: 0.5,
+    cacheTime: 5 * 60 * 1000,
     enabled: !!selectedSpecie,
     onError: (error: any) => {
       console.error("Error fetching Star Wars data:", error);
     },
   });
 
-  const rows = (species.results as ISpecie[])?.map(
+  const handleSelectedIndex = (url: string) => {
+    setSelectedSpecie(url.toString());
+  };
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.currentTarget.value);
+  };
+
+  const filteredSpeceis = useMemo(() => {
+    if (!search) return (species.results as ISpecie[]) || [];
+    return (species.results as ISpecie[]).filter((specie) =>
+      specie.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [search, species.results]);
+
+  const rows = (filteredSpeceis as ISpecie[])?.map(
     (specie: ISpecie, index: number) => (
       <tr
         key={index}
         style={{ cursor: "pointer" }}
         onClick={() => handleSelectedIndex(specie.url)}
       >
-        <td>{specie?.name}</td>
-        <td>{specie?.designation}</td>
-        <td>{specie?.language}</td>
-        <td>{specie?.average_lifespan}</td>
-        <td>{specie?.average_height}</td>
-        <td>{specie?.skin_colors}</td>
+        <td>
+          <Text lineClamp={1}>{specie?.name}</Text>
+        </td>
+        <td>
+          <Text lineClamp={1}>{specie?.designation}</Text>
+        </td>
+        <td>
+          <Text lineClamp={1}>{specie?.language}</Text>
+        </td>
+        <td>
+          <Text lineClamp={1}>{specie?.average_lifespan}</Text>
+        </td>
+        <td>
+          <Text lineClamp={1}>{specie?.average_height}</Text>
+        </td>
+        <td>
+          <Text lineClamp={1}>{specie?.skin_colors}</Text>
+        </td>
       </tr>
     )
   );
 
-  const handleSelectedIndex = (url: string) => {
-    setSelectedSpecie(url.toString());
-  };
-
-  return (
+  return speciesLoading || selectedSpecieLoading ? (
+    <Center w="100vw" h="80vh">
+      <Loader size="xl" />
+    </Center>
+  ) : (
     <Container pt={24}>
-      <Title>
-        <Text weight="bold" size={"lg"}>
-          Species
-        </Text>
-      </Title>
+      <Flex pb={8} justify={"end"}>
+        <Button
+          onClick={() => navigate("/")}
+          variant="outline"
+          leftIcon={<MdChevronLeft size={20} />}
+        >
+          Go Back
+        </Button>
+      </Flex>
+      <Flex align="center" justify="space-between">
+        <Title>
+          <Text weight="bold" size={"lg"}>
+            Species
+          </Text>
+        </Title>
+        <Input
+          icon={<MdSearch scale={20} />}
+          placeholder="Search"
+          value={search}
+          onChange={handleSearch}
+        />
+      </Flex>
       <Table highlightOnHover>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Designation</th>
-            <th>Language</th>
-            <th>Average Life Span</th>
-            <th>Average Height</th>
-            <th>Skin Colors</th>
+            <th>
+              <Text lineClamp={1}>Name</Text>
+            </th>
+            <th>
+              <Text lineClamp={1}>Designation</Text>
+            </th>
+            <th>
+              <Text lineClamp={1}>Language</Text>
+            </th>
+            <th>
+              <Text lineClamp={1}>Average Life Span</Text>
+            </th>
+            <th>
+              <Text lineClamp={1}>Average Height</Text>
+            </th>
+            <th>
+              <Text lineClamp={1}>Skin Colors</Text>
+            </th>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
--- src/pages/Species/SpeciesDetails.tsx
+++ src/pages/Species/SpeciesDetails.tsx
@@ -1,28 +1,38 @@
 import {
   Badge,
+  Box,
+  Button,
   Center,
   Container,
   Flex,
   Loader,
+  Tabs,
   Text,
   Title,
 } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import { FC } from "react";
-import { useParams } from "react-router-dom";
+import { GiInterceptorShip } from "react-icons/gi";
+import { IoMdFilm, IoMdPeople, IoMdPerson } from "react-icons/io";
+import { MdChevronLeft } from "react-icons/md";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import FilmDataCard from "../../components/FilmDataCard";
+import PeoplesDataCard from "../../components/PeoplesDataCard";
 import { getFilmsById, getPeopleById } from "../../services/api";
 import { useAppStore } from "../../store/app.store";
 
 const SpeciesDetails: FC = () => {
   const { specieDetail } = useAppStore();
-  const { id } = useParams();
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const activeTab = searchParams.get("tab") || "info";
 
   const { data: filmsData, isLoading: filmLoading } = useQuery({
-    queryKey: ["people_film"],
+    queryKey: ["species_film"],
     queryFn: () => {
       const filmsPromise = specieDetail.films.map(async (filmUrl) => {
         const film = await getFilmsById(filmUrl.toString());
-        return film.title;
+        return film;
       });
 
       return Promise.all(filmsPromise);
@@ -30,99 +40,156 @@ const SpeciesDetails: FC = () => {
   });
 
   const { data: peoplesData, isLoading: peoplesLoading } = useQuery({
-    queryKey: ["planets_peoples"],
+    queryKey: ["species_peoples"],
     queryFn: () => {
       const peoplesPromise = specieDetail.people.map(async (character) => {
         const people = await getPeopleById(character.toString());
-        return people.name;
+        return people;
       });
 
       return Promise.all(peoplesPromise);
     },
   });
 
+  const handleTabChange = (tab: string) => {
+    setSearchParams({ tab });
+  };
+
   return (
     <Container pt={24}>
-      <Title pb={18}>
-        <Text>Specie Details</Text>
-      </Title>
-      {peoplesLoading && filmLoading && (
-        <Center w="100vw" h="80vh">
-          <Loader size="xl" />
-        </Center>
-      )}
-      <Flex direction="column" align="start" gap={12} justify="center">
-        <Flex align="center" justify="start" gap={8} direction="row">
-          <Text weight="bolder" size="lg">
-            Name:
-          </Text>
-          <Text weight="bolder" size="lg" transform="capitalize">
-            {specieDetail.name}
-          </Text>
-        </Flex>
-        <Flex align="center" justify="start" gap={8} direction="row">
-          <Text>Designation:</Text>
-          <Text transform="capitalize">{specieDetail.designation}</Text>
-        </Flex>
-        <Flex align="center" justify="start" gap={8} direction="row">
-          <Text>Classification:</Text>
-          <Text transform="capitalize">{specieDetail.classification}</Text>
-        </Flex>
-        <Flex align="center" justify="start" gap={8} direction="row">
-          <Text>Eye Color:</Text>
-          <Text transform="capitalize">{specieDetail.eye_colors}</Text>
-        </Flex>
-        <Flex align="center" justify="start" gap={8} direction="row">
-          <Text>Hair Color:</Text>
-          <Text transform="capitalize">{specieDetail.hair_colors}</Text>
-        </Flex>
-        <Flex align="center" justify="start" gap={8} direction="row">
-          <Text>Average Height:</Text>
-          <Text transform="capitalize">{specieDetail.average_height}</Text>
-        </Flex>
-        <Flex align="center" justify="start" gap={8} direction="row">
-          <Text>Average Life Span:</Text>
-          <Text transform="capitalize">{specieDetail.average_lifespan}</Text>
-        </Flex>
-        <Flex gap={6} pb={12} wrap={"wrap"} w="100vw" align="center">
-          <Text>Films: </Text>
-          <Flex wrap={"wrap"} gap={8} align="center" justify="center">
-            {filmsData && filmsData?.length > 0 ? (
-              filmsData.map((film, index) => (
-                <Badge
-                  variant="gradient"
-                  gradient={{ from: "indigo", to: "cyan" }}
-                  p={12}
-                  key={index}
-                >
-                  {film}
-                </Badge>
-              ))
-            ) : (
-              <Text>N/A</Text>
-            )}
-          </Flex>
-        </Flex>
-        <Flex gap={6} pb={12}>
-          <Text>Peoples: </Text>
-          <Flex wrap={"wrap"} gap={8} align="center" justify="center">
-            {peoplesData && peoplesData?.length > 0 ? (
-              peoplesData.map((people, index) => (
-                <Badge
-                  variant="gradient"
-                  gradient={{ from: "teal", to: "lime", deg: 105 }}
-                  p={12}
-                  key={index}
-                >
-                  {people}
-                </Badge>
-              ))
-            ) : (
-              <Text>N/A</Text>
-            )}
-          </Flex>
-        </Flex>
+      <Flex
+        align={{ base: "center" }}
+        justify={{ base: "space-between" }}
+        direction={{ base: "row" }}
+      >
+        <Title pt={16} fz={{ base: "md", lg: "xl" }} pb={18}>
+          <Text>Specie Details</Text>
+        </Title>
+        <Button
+          onClick={() => navigate("/species")}
+          variant="outline"
+          leftIcon={<MdChevronLeft size={20} />}
+        >
+          Go Back
+        </Button>
       </Flex>
+      <Tabs defaultValue={activeTab} onTabChange={handleTabChange}>
+        <Tabs.List>
+          <Tabs.Tab value="info" icon={<IoMdPerson size={22} />}>
+            <Text>Info</Text>
+          </Tabs.Tab>
+          <Tabs.Tab value="films" icon={<IoMdFilm size={22} />}>
+            Films
+          </Tabs.Tab>
+          <Tabs.Tab value="peoples" icon={<IoMdPeople size={22} />}>
+            Peoples
+          </Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value="info" pt="xs">
+          <Box pt={12}>
+            <Title pb={12} fz={{ base: "md", lg: "xl" }}>
+              <Text>Personal Info</Text>
+            </Title>
+            <Flex
+              fz={{ base: "sm", lg: "lg" }}
+              align="center"
+              justify="start"
+              gap={8}
+              direction="row"
+            >
+              <Text weight="bold">Name:</Text>
+              <Text transform="capitalize">{specieDetail.name}</Text>
+            </Flex>
+            <Flex
+              fz={{ base: "sm", lg: "lg" }}
+              align="center"
+              justify="start"
+              gap={8}
+              direction="row"
+            >
+              <Text weight="bold">Designation:</Text>
+              <Text transform="capitalize">{specieDetail.designation}</Text>
+            </Flex>
+            <Flex
+              fz={{ base: "sm", lg: "lg" }}
+              align="center"
+              justify="start"
+              gap={8}
+              direction="row"
+            >
+              <Text weight="bold">Classification:</Text>
+              <Text transform="capitalize">{specieDetail.classification}</Text>
+            </Flex>
+            <Flex
+              fz={{ base: "sm", lg: "lg" }}
+              align="center"
+              justify="start"
+              gap={8}
+              direction="row"
+            >
+              <Text weight="bold">Eye Color:</Text>
+              <Text transform="capitalize">{specieDetail.eye_colors}</Text>
+            </Flex>
+            <Flex
+              fz={{ base: "sm", lg: "lg" }}
+              align="center"
+              justify="start"
+              gap={8}
+              direction="row"
+            >
+              <Text weight="bold">Hair Color:</Text>
+              <Text transform="capitalize">{specieDetail.hair_colors}</Text>
+            </Flex>
+            <Flex
+              fz={{ base: "sm", lg: "lg" }}
+              align="center"
+              justify="start"
+              gap={8}
+              direction="row"
+            >
+              <Text weight="bold">Average Height:</Text>
+              <Text transform="capitalize">{specieDetail.average_height}</Text>
+            </Flex>
+            <Flex
+              fz={{ base: "sm", lg: "lg" }}
+              align="center"
+              justify="start"
+              gap={8}
+              direction="row"
+            >
+              <Text weight="bold">Average Life Span:</Text>
+              <Text transform="capitalize">
+                {specieDetail.average_lifespan}
+              </Text>
+            </Flex>
+          </Box>
+        </Tabs.Panel>
+
+        <Tabs.Panel value="films" pt="xs">
+          <Box pt={12}>
+            <Title pb={12} fz={{ base: "md", lg: "xl" }}>
+              <Text>Film Info</Text>
+            </Title>
+            <FilmDataCard
+              isFilmDataLoading={filmLoading}
+              filmsData={filmsData}
+            />
+          </Box>
+        </Tabs.Panel>
+
+        <Tabs.Panel value="peoples" pt="xs">
+          <Box pt={12}>
+            <Title pb={12} fz={{ base: "md", lg: "xl" }}>
+              <Text>Peoples Info</Text>
+            </Title>
+            <PeoplesDataCard
+              ispeoplesDataLoading={peoplesLoading}
+              peoplesData={peoplesData}
+            />
+          </Box>
+        </Tabs.Panel>
+      </Tabs>
     </Container>
   );
 };
